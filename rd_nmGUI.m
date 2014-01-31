01dfbd7c74c2ed9a77579832d8c9913809c14c75
Rachel Denison <r.n.denison@gmail.com>
2014-01-31 15:58:49 -0500

    added warning message about not calling rd_nmGUI.m with the appropriate output arguments

--- rd_nmGUI.m
+++ rd_nmGUI.m
@@ -1,20 +1,27 @@
 function [nm opts R] = rd_nmGUI(opts)
 %
-% function [nm R] = rd_nmGUI(opts)
+% function [nm opts R] = rd_nmGUI(opts)
 %
 % INPUTS:
 % opts is an options structure for the model (optional)
 % 
 % OUTPUTS:
-% nm is the gui structure. This is a *required* output argument for the gui
-% to work.
-% opts is the options structure. This is also a *required* output argument 
-%   for the gui to work.
+% nm is the gui structure. This is a required output argument for the gui
+%   to work, and it must be called 'nm'.**
+% opts is the options structure. This is also a required output argument 
+%   for the gui to work, and it must be called 'opts'.**
+% ** The truth is, these variables just have to exist in the workspace from
+%   which you called this function. This is because the refresh functions
+%   call these variables from the workspace. But their values don't matter.
 % R is the current response of the model (1 by length of x) and is
 % optional.
 %
 % Rachel Denison
 % Jan 2014
+
+if nargout < 2
+    warning('Be aware! If you did not call this function with nm and opts as output args, they must already exist in the workspace (see the help for this function).')
+end
 
 if notDefined('opts')
     opts = [];
